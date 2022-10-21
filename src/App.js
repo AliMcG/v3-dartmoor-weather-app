babfab035ce0b3b08978381ee83ce05144c4b10f
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-10-21 16:50:47 +0100

    refactored code to use only one perice of state for co-ordinates

--- src/App.js
+++ src/App.js
@@ -9,39 +9,38 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  // starting co-ordinates to centre map on Dartmoor.
   const position = { lat: 50.556, lng: -3.959884643554688 };
+  // starting empty object for co-ordinates data.
   const initialCoords = {
-    longitude: "",
-    lat: ""
+    longi: "",
+    lati: ""
   }
   const [coordinates, setCoordinates] = useState(initialCoords)
-  const [longitude, setLongitude] = useState("");
-  const [latitude, setLatitude] = useState("");
   const [weather, setWeather] = useState(false);
   const [weatherData, setWeatherData] = useState(null);
 
   async function getWeather() {
+    console.log("coords inside getWeather", coordinates)
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates?.lati}&lon=${coordinates?.longi}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
     );
     const data = await response.json();
-    console.log(data);
     setWeatherData(data);
-    console.log("fetch weather data");
   }
 
   function LocationMarker() {
     const map = useMapEvents({
       click(e) {
-        setLongitude(e.latlng.lng);
-        setLatitude(e.latlng.lat);
-        // setPosition(e.latlng)
-        console.log(e.latlng.lng);
-        console.log(e.latlng.lat);
-        console.log(`longitude: `, longitude);
-        console.log(`latitude: `, latitude);
+        // deconstructs the event.object to get the lng and lat.
+        const { lng, lat }  = e.latlng
+        // writes the new co-ordinates to state.
+        setCoordinates(() => {
+          return { longi: lng, lati: lat};
+        })
+        // true/false state to control conditional render.
         setWeather(true);
-        if (longitude) {
+        if (coordinates.longi) {
           getWeather();
         }
       },
@@ -56,10 +55,10 @@ function App() {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {weather && (
-          <Marker position={[latitude, longitude]}>
+          <Marker position={[coordinates?.lati, coordinates?.longi]}>
             <Popup>
-              <p>Longitude: {longitude}</p>
-              <p>Latitude: {latitude}</p>
+              <p>Longitude: {coordinates?.long}</p>
+              <p>Latitude: {coordinates?.lati}</p>
               <p>Current temperature: {weatherData?.main.temp}°C.</p>
               <p>{weatherData?.weather[0].description}</p>
             </Popup>
