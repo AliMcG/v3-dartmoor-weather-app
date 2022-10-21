5a72b4f6842c9c4863b82aa84064fc2634f4112e
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-10-21 20:32:42 +0100

    report aws .file

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "v3dartmoorweatherapp": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
